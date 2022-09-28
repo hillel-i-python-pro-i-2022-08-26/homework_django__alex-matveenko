6e8263b05ceff387b902d1b1e518703574947191
Alex-Matveenko <matveenko.a.p@gmail.com>
2022-09-28 18:24:10 +0300

    create Makefile and fix pre-commit

--- greetings/urls.py
+++ greetings/urls.py
@@ -2,7 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path("", views.greetings),
-    path("<str:name>", views.greetings)
-]
+urlpatterns = [path("", views.greetings), path("<str:name>", views.greetings)]
