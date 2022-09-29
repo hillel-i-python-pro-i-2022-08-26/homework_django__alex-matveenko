b107baae795e177685f778eda31af053325ced10
Alex-Matveenko <matveenko.a.p@gmail.com>
2022-09-29 20:07:08 +0300

    fix view name when reload page and add comma to list

--- greetings/views.py
+++ greetings/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse, HttpRequest
 from core.services.generate_name import generate_name
 
 
-def greetings(request: HttpRequest, name: str = generate_name()) -> HttpResponse:
-    return HttpResponse(f"Hello, {name.title()}!")
+def greetings(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f'Hello, {generate_name()}!')
+
+
+def self_greetings(request: HttpRequest, name: str) -> HttpResponse:
+    return HttpResponse(f'Hello, {name.title()}!')
