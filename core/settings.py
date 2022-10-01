3a79e50753f88bebd4524c70d2ae05feddf41542
Alex-Matveenko <matveenko.a.p@gmail.com>
2022-10-01 14:17:36 +0300

    Remade greetings app with templates

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-g(m)%8=7!o9_dl()r@b873ufk73cr==)_h3te2d_qipxkl++9k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "greetings.apps.GreetingsConfig",
 ]
 
