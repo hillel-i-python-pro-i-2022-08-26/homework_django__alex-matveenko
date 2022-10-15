5b9017f7312f968426d7d0cef0021bce77e838cc
Alex-Matveenko <matveenko.a.p@gmail.com>
2022-10-15 20:56:48 +0300

    create session app

---
+++ session_app/views.py
@@ -0,0 +1,32 @@
+import datetime
+import uuid
+
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+
+def get_session_info(request: WSGIRequest | HttpRequest) -> HttpResponse:
+    session = request.session
+    # Set_session_info__start
+    time_of_visit = session.get("time_of_visit", datetime.datetime.now())
+    session_id = session.get("session_id", uuid.uuid4())
+    count_of_visits = session.get("count", 0)
+    # Set_session_info__stop
+
+    count_of_visits += 1
+
+    # Return_data_to_session
+    session["session_id"] = str(session_id)
+    session["count"] = count_of_visits
+
+    return render(
+        request,
+        "session_app/session_view.html",
+        {
+            "title": "Session Info",
+            "session_id": session_id,
+            "count_of_visits": count_of_visits,
+            "session_time": time_of_visit,
+        },
+    )
