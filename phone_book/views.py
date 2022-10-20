db9dfe2643b21276bf715a52d3b2ee5da68f030a
Alex-Matveenko <matveenko.a.p@gmail.com>
2022-10-20 21:34:30 +0300

    change name of model and add command to delete duplicate names

--- phone_book/views.py
+++ phone_book/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from phone_book.models import PhoneBook
+from phone_book.models import Contacts
 
 
 def show_contacts(request: HttpRequest) -> HttpResponse:
-    contacts = PhoneBook.objects.all()
+    contacts = Contacts.objects.all()
     return render(
         request,
         "phone_book/index.html",
