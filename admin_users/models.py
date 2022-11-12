cfeb7379bb85b389477a9d3e286267f71d64b6e8
Alex-Matveenko <matveenko.a.p@gmail.com>
2022-11-12 12:20:26 +0200

    add some changes in model for user

--- admin_users/models.py
+++ admin_users/models.py
@@ -1,7 +1,29 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.urls import reverse
+from ulid import ULID
+
+
+def get_user_avatar_path(instance, filename: str) -> str:
+    ulid = ULID()
+    _, extension = filename.rsplit(".", maxsplit=1)
+    return f"users/user/avatar/{ulid.to_uuid()}/avatar.{extension}"
 
 
 class AdminUser(AbstractUser):
+    avatar = models.ImageField(
+        max_length=255,
+        blank=True,
+        null=True,
+        upload_to=get_user_avatar_path,
+    )
+
+    def get_absolute_url(self):
+        return reverse(
+            "account:edit",
+            kwargs={"pk": self.pk},
+        )
+
     class Meta:
         verbose_name = "User"
         verbose_name_plural = "Admin User"
