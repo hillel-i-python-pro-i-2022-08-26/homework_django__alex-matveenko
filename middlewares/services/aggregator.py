e780c26f9ff31f4ca31feaffdff60c8093af8ae8
Alex-Matveenko <matveenko.a.p@gmail.com>
2022-11-29 22:37:26 +0200

    refactor code

---
+++ middlewares/services/aggregator.py
@@ -0,0 +1,10 @@
+from django.db.models import Sum, Count
+
+
+def aggregator(queryset):
+    total_visits = queryset.aggregate(Sum('count_of_visits')).get('count_of_visits__sum')
+    total_pages = queryset.aggregate(Count('path')).get('path__count')
+    return {
+        "total_visits": total_visits,
+        "count_of_visited_pages": total_pages,
+    }
